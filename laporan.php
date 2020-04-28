7e32d897f7e94dd6d4d61e1de06cf1e424480d96
Rafli Ramadhan <rafliramadhan025@gmail.com>
2020-04-28 10:29:39 +0700

    Create laporan.php

---
+++ laporan.php
@@ -0,0 +1,7 @@
+<?php
+include "header.php"; ?>
+<h3>laporan</h3>
+<ul>
+	<li><a href="laporan_data_guru.php" target="_blank">laporan Data Guru</a></li>
+	<li><a href="laporan_data_siswa.php" target="_blank">laporan Data Siswa</a></li>
+</ul>
